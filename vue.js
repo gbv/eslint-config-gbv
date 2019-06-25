6511d9e12f6657943211a14392f77b47125dc252
Stefan Peters <Stefan.Peters@gbv.de>
2019-06-25 08:17:42 +0200

    Initial commit

---
+++ vue.js
@@ -0,0 +1,25 @@
+module.exports = {
+  "extends": [
+    "plugin:vue/recommended",
+  ],
+  "rules": {
+    // We need v-html at some places.
+    "vue/no-v-html": "off",
+    // Closing bracket formatting.
+    "vue/html-closing-bracket-newline": [
+      "error",
+      {
+        "singleline": "never",
+        "multiline": "never",
+      },
+    ],
+    "vue/html-closing-bracket-spacing": [
+      "error",
+      {
+        "startTag": "never",
+        "endTag": "never",
+        "selfClosingTag": "always",
+      },
+    ],
+  },
+}
